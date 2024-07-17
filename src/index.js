ec2b4a0abf3a5587d76bfe7ba9f79fed8a059296
Le Vi <lethaivi.work@gmail.com>
2024-07-17 14:10:10 +0700

    init commit

---
+++ src/index.js
@@ -0,0 +1,128 @@
+const restify = require("restify");
+const server = restify.createServer();
+server.use(restify.plugins.bodyParser());
+server.use(restify.plugins.queryParser());
+
+const VERIFY_TOKEN = process.env.VERIFY_TOKEN;
+const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
+const PAGE_ID = process.env.PAGE_ID;
+
+server.post("/webhook", async (req, res) => {
+  let body = req.body;
+  res.send(200, { message: "EVENT_RECEIVED" });
+  const data = body.entry[0];
+
+  console.log(
+    `messenger-${data.messaging[0].sender.id}-${data.messaging[0].recipient.id}`
+  );
+  console.log(`\u{1F7EA} Received webhook:`);
+  console.dir(body, { depth: null });
+  if (body.object === "page") {
+    // Returns a '200 OK' response to all requests
+    console.log(body);
+    await fetch("http://localhost:3978/api/messages", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        text: data.messaging[0].message.text,
+        textFormat: "plain",
+        type: "message",
+        channelId: "messenger",
+        from: {
+          id: data.messaging[0].sender.id,
+          name: "User",
+          role: "user",
+        },
+        conversation: {
+          id: `messenger-${data.messaging[0].sender.id}-${data.messaging[0].recipient.id}`,
+        },
+        id: `messenger-${data.messaging[0].sender.id}-${data.messaging[0].recipient.id}`,
+        recipient: {
+          id: data.messaging[0].recipient.id,
+          name: "Bot",
+          role: "bot",
+        },
+        serviceUrl: "http://localhost:3000",
+      }),
+    });
+  } else {
+    // Return a '404 Not Found' if event is not from a page subscription
+    res.send(404);
+  }
+});
+
+server.get("/webhook", async (req, res) => {
+  let mode = req.query["hub.mode"];
+  let token = req.query["hub.verify_token"];
+  let challenge = req.query["hub.challenge"];
+
+  // Check if a token and mode is in the query string of the request
+  if (mode && token) {
+    // Check the mode and token sent is correct
+    if (mode === "subscribe" && token === VERIFY_TOKEN) {
+      // Respond with the challenge token from the request
+      console.log("WEBHOOK_VERIFIED");
+      console.log(challenge);
+      res.send(200, +challenge);
+    } else {
+      res.send(403);
+    }
+  }
+});
+
+server.post("/proactive/:conversationId", async (req, res) => {
+  res.send(200);
+  const conversationId = req.params.conversationId;
+  const senderId = conversationId.split("-")[1];
+  await fetch(
+    `https://graph.facebook.com/v20.0/${PAGE_ID}/messages?access_token=${PAGE_ACCESS_TOKEN}`,
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        recipient: {
+          id: senderId,
+        },
+        messaging_type: "RESPONSE",
+        message: {
+          text: "Proactive messages",
+        },
+      }),
+    }
+  );
+});
+
+server.post(
+  "/v3/conversations/:conversationId/activities/:conversationId",
+  async (req, res) => {
+    res.send(200);
+    const conversationId = req.params.conversationId;
+    const senderId = conversationId.split("-")[1];
+    await fetch(
+      `https://graph.facebook.com/v20.0/${PAGE_ID}/messages?access_token=${PAGE_ACCESS_TOKEN}`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          recipient: {
+            id: senderId,
+          },
+          messaging_type: "RESPONSE",
+          message: {
+            text: req.body.text,
+          },
+        }),
+      }
+    );
+  }
+);
+
+server.listen(3000, function () {
+  console.log("%s listening at %s", server.name, server.url);
+});
